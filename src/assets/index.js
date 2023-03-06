b7479cf8c4eb4c4f586aebf4a5492d17b8e776b4
PovilasGe <povilasgedminas@gmail.com>
2023-03-06 23:38:09 +0200

    add experience

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,11 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+
+import bct from "./company/bct.png";
+import codeacademy from "./company/codeacademy.png";
+import se from "./company/se.png";
+import nerdwelten from "./company/nerdwelten.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +53,10 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  bct,
+  codeacademy,
+  nerdwelten,
+  se,
   carrent,
   jobit,
   tripguide,
