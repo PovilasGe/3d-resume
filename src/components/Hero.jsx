3ae9a9e00eb0cfda9f46ae7f1f11c4dbd84d97ea
PovilasGe <povilasgedminas@gmail.com>
2023-03-13 15:42:13 +0200

    fix computer

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,10 +31,10 @@ const Hero = () => {
           )}
         </div>
       </div>
-      {/* 
+
       {!isMobile && <ComputersCanvas />}
 
-      {isMobile && (
+      {/* {isMobile && (
         <img
           src={computer}
           alt='Computer'
